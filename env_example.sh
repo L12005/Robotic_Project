d60e8440895f9e58f53070e6294cb9b5fb649242
orange <fadaiorange@gmail.com>
2026-05-01 18:44:51 +0800

    添加场景

--- env_example.sh
+++ env_example.sh
@@ -1,1 +1,2 @@
-export GZ_SIM_RESOURCE_PATH=/home/username/robotic_project/ws:${GZ_SIM_RESOURCE_PATH}
+export HMI_PROJECT_ROOT=/path/to/Robotic_Project
+export GZ_SIM_RESOURCE_PATH=${HMI_PROJECT_ROOT}/src/hmi_elements:${GZ_SIM_RESOURCE_PATH}
